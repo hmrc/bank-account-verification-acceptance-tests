1adfc74bf48c21b9c9084568c0b69602244b7324
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-08-12 10:30:26 +0100

    TAV-75 Remove @Ignore tag that snuck in there

--- src/test/scala/uk/gov/hmrc/acceptance/spec/ZapSpec.scala
+++ src/test/scala/uk/gov/hmrc/acceptance/spec/ZapSpec.scala
@@ -1,13 +1,11 @@
 package uk.gov.hmrc.acceptance.spec
 
 import com.typesafe.config.{Config, ConfigFactory}
-import org.scalatest.Ignore
 import org.scalatest.featurespec.AnyFeatureSpec
 import uk.gov.hmrc.acceptance.tags.Zap
 import uk.gov.hmrc.zap.ZapTest
 import uk.gov.hmrc.zap.config.ZapConfiguration
 
-@Ignore
 class ZapSpec extends AnyFeatureSpec with ZapTest {
   val zapConfig: Config = ConfigFactory.load().getConfig("zap-automation-config")
 
