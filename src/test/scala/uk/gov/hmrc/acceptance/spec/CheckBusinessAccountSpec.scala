0755da519e0f6a48d2b007a6b781b722e0cda90f
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-09-10 15:11:31 +0100

    #TAV-99 Add/Update tests
    Fix closed bank account scenario
    Add unable to find bank account scenario

--- src/test/scala/uk/gov/hmrc/acceptance/spec/CheckBusinessAccountSpec.scala
+++ src/test/scala/uk/gov/hmrc/acceptance/spec/CheckBusinessAccountSpec.scala
@@ -1,9 +1,11 @@
 package uk.gov.hmrc.acceptance.spec
 
+import java.util.UUID.randomUUID
+
 import org.assertj.core.api.Assertions.assertThat
 import org.mockserver.model.{HttpRequest, HttpResponse}
 import uk.gov.hmrc.acceptance.config.TestConfig
-import uk.gov.hmrc.acceptance.pages.{BusinessAccountEntryPage, ConfirmDetailsPage, ExampleFrontendDonePage, PersonalAccountEntryPage, SelectAccountTypePage}
+import uk.gov.hmrc.acceptance.pages._
 import uk.gov.hmrc.acceptance.utils.{BaseSpec, MockServer}
 
 class CheckBusinessAccountSpec extends BaseSpec with MockServer {
@@ -107,7 +109,7 @@ class CheckBusinessAccountSpec extends BaseSpec with MockServer {
     assertThat(ExampleFrontendDonePage().getAccountExists).isEqualTo("yes")
   }
 
-  Scenario("Business Bank Account Verification invalid bank account") {
+  Scenario("Business Bank Account Verification closed bank account") {
     mockServer.when(
       HttpRequest.request()
         .withMethod("POST")
@@ -115,11 +117,11 @@ class CheckBusinessAccountSpec extends BaseSpec with MockServer {
     ).respond(
       HttpResponse.response()
         .withHeader("Content-Type", "application/json")
-        .withBody(s"""{"Matched": false, "ReasonCode": "AC01"""".stripMargin)
+        .withBody(s"""{"Matched": false, "ReasonCode": "AC01"}""".stripMargin)
         .withStatusCode(200)
     )
 
-    Given("I want to collect and validate a customers bank account details")
+    Given("I want to collect and validate a companies bank account details")
 
     val journeyId: String = initializeJourney()
     go to journeyStartPage(journeyId)
@@ -128,7 +130,54 @@ class CheckBusinessAccountSpec extends BaseSpec with MockServer {
 
     SelectAccountTypePage().selectBusinessAccount().clickContinue()
 
-    When("a customer enters all required information and clicks continue")
+    When("a company representative enters all required information and clicks continue")
+
+    BusinessAccountEntryPage()
+      .enterCompanyName("Account Closed")
+      .enterCompanyRegistrationNumber(DEFAULT_COMPANY_REGISTRATION_NUMBER)
+      .enterSortCode(DEFAULT_BANK_SORT_CODE)
+      .enterAccountNumber(DEFAULT_BANK_ACCOUNT_NUMBER)
+      .clickContinue()
+
+    Then("an error message is displayed to the company representative telling them that the account is invalid")
+
+    assertThat(PersonalAccountEntryPage().errorMessageSummaryCount()).isEqualTo(1)
+    assertThatErrorSummaryLinkExists("accountNumber")
+    assertThatInputFieldErrorMessageExists("accountNumber")
+  }
+
+  Scenario("Business Bank Account Verification unable to find bank account") {
+    mockServer.when(
+      HttpRequest.request()
+        .withMethod("POST")
+        .withPath(SUREPAY_PATH)
+    ).respond(
+      HttpResponse.response()
+        .withHeader("Content-Type", "application/json")
+        .withBody(s"""{"Matched": false, "ReasonCode": "SCNS"}""".stripMargin)
+        .withStatusCode(200)
+    )
+    mockServer.when(
+      HttpRequest.request()
+        .withMethod("POST")
+        .withPath(CREDITSAFE_PATH)
+    ).respond(
+      HttpResponse.response()
+        .withHeader("Content-Type", "text/plain")
+        .withBody(s"""{"requestId":"${randomUUID().toString}","result":"none","isActive":true,"confidence":{}}""".stripMargin)
+        .withStatusCode(200)
+    )
+
+    Given("I want to collect and validate a companies bank account details")
+
+    val journeyId: String = initializeJourney()
+    go to journeyStartPage(journeyId)
+
+    assertThat(SelectAccountTypePage().isOnPage).isTrue
+
+    SelectAccountTypePage().selectBusinessAccount().clickContinue()
+
+    When("a company representative enters all required information and clicks continue")
 
     BusinessAccountEntryPage()
       .enterCompanyName("Cannot Match")
@@ -137,7 +186,7 @@ class CheckBusinessAccountSpec extends BaseSpec with MockServer {
       .enterAccountNumber(DEFAULT_BANK_ACCOUNT_NUMBER)
       .clickContinue()
 
-    Then("the customer is redirected to the confirm account screen")
+    Then("the company representative is redirected to the confirm account screen")
 
     assertThat(ConfirmDetailsPage().isOnPage).isTrue
     assertThat(ConfirmDetailsPage().getCompanyName).isEqualTo("Cannot Match")
@@ -148,7 +197,7 @@ class CheckBusinessAccountSpec extends BaseSpec with MockServer {
 
     ConfirmDetailsPage().clickContinue()
 
-    Then("the customer is redirected to continue URL")
+    Then("the company representative is redirected to the continue URL")
 
     assertThat(webDriver.getCurrentUrl).isEqualTo(s"${TestConfig.url("bank-account-verification-frontend-example")}/done/$journeyId")
     assertThat(ExampleFrontendDonePage().getAccountType).isEqualTo("business")
@@ -158,9 +207,9 @@ class CheckBusinessAccountSpec extends BaseSpec with MockServer {
     assertThat(ExampleFrontendDonePage().getRollNumber).isEmpty()
     assertThat(ExampleFrontendDonePage().getValidationResult).isEqualTo("yes")
     assertThat(ExampleFrontendDonePage().getCompanyNameMatches).isEqualTo("indeterminate")
-    assertThat(ExampleFrontendDonePage().getCompanyPostcodeMatches).isEqualTo("indeterminate")
+    assertThat(ExampleFrontendDonePage().getCompanyPostcodeMatches).isEqualTo("inapplicable")
     assertThat(ExampleFrontendDonePage().getCompanyRegistrationNumberMatches).isEqualTo("indeterminate")
-    assertThat(ExampleFrontendDonePage().getAccountExists).isEqualTo("no")
+    assertThat(ExampleFrontendDonePage().getAccountExists).isEqualTo("indeterminate")
   }
 
 }
