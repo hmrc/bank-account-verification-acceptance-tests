1507ecd81ed2ffaa8df7fcbea8d584973fa039a3
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-10-08 11:55:16 +0100

    Tweak zap shell script

--- run-zap-specs.sh
+++ run-zap-specs.sh
@@ -6,17 +6,16 @@
 
 DEFAULT_BROWSER=remote-chrome
 BROWSER_TYPE=$1
-ENV=$2
 
 if [ -z "$BROWSER_TYPE" ]; then
     echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
     echo ""
 fi
 
-sbt -Dbrowser=${BROWSER_TYPE:=$DEFAULT_BROWSER} -Denv=${ENV:=local} -Dzap.proxy=true "testOnly -- -l uk.gov.hmrc.acceptance.tags.Zap"
+sbt -Dbrowser=${BROWSER_TYPE:=$DEFAULT_BROWSER} -Dzap.proxy=true "testOnly -- -n uk.gov.hmrc.acceptance.tags.Zap"
 
 echo ""
 echo "**** Running ZapSpec ****"
 echo ""
 
-sbt "testOnly -- -n uk.gov.hmrc.acceptance.tags.Zap"
+sbt "testOnly uk.gov.hmrc.acceptance.spec.ZapSpec"
