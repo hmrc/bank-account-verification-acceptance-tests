314ee827e1a2547d3acd7d901f9eae019589a854
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-10-13 09:41:13 +0100

    Don't extend base spec, not required

--- src/test/scala/uk/gov/hmrc/acceptance/spec/ZapSpec.scala
+++ src/test/scala/uk/gov/hmrc/acceptance/spec/ZapSpec.scala
@@ -1,12 +1,11 @@
 package uk.gov.hmrc.acceptance.spec
 
 import com.typesafe.config.{Config, ConfigFactory}
-import uk.gov.hmrc.acceptance.tags.Zap
-import uk.gov.hmrc.acceptance.utils.{BaseSpec, MockServer}
+import org.scalatest.featurespec.AnyFeatureSpec
 import uk.gov.hmrc.zap.ZapTest
 import uk.gov.hmrc.zap.config.ZapConfiguration
 
-class ZapSpec extends BaseSpec with MockServer with ZapTest {
+class ZapSpec extends AnyFeatureSpec with ZapTest {
   val zapConfig: Config = ConfigFactory.load().getConfig("zap-automation-config")
 
   override val zapConfiguration: ZapConfiguration = new ZapConfiguration(zapConfig)
@@ -15,4 +14,3 @@ class ZapSpec extends BaseSpec with MockServer with ZapTest {
     triggerZapScan()
   }
 }
-
