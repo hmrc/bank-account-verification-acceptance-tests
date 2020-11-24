80bc8f016d3807c07cdd2cfcae4f935839d1bef0
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-11-24 12:22:23 +0000

    #TAV-150 add control scripts
    Add rinetd to container to allow port sharing on mac
    Add script to build Dockerfile
    Add script to start up zap container

---
+++ build-zap-container.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build --tag zap-with-rinetd:2.9.0-0.1.0 docker/zap/.
--- run-browser-with-docker.sh
+++ run-browser-with-docker.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-DEFAULT_BROWSER=remote-chrome
-ZAP_VERSION="2.9.0"
-ZAP_CONTAINER="artefacts.tax.service.gov.uk/build-zap:${ZAP_VERSION}-latest"
-BROWSER_TYPE=${1:-$DEFAULT_BROWSER}
-
 #######################################
 # The script starts remote-chrome or remote-firefox docker container for running Browser tests on a developer machine only.
 # The container directs TCP requests from the container to the host machine enabling testing services running via Service Manager.
@@ -29,19 +24,28 @@ port_mappings="$port_mappings,11000->11000"
 #port_mappings="9032->9032,9250->9250,9080->9080"
 
 #######################################
-# Defines the CONTAINER variable from the argument passed to the script
+# Defines the BROWSER variable from the argument passed to the script
 #######################################
-if [ "${BROWSER_TYPE}" = "remote-chrome" ]; then
-  CONTAINER="artefacts.tax.service.gov.uk/chrome-with-rinetd:83.0.4103.61-latest"
-elif [ "${BROWSER_TYPE}" = "remote-firefox" ]; then
-  CONTAINER="artefacts.tax.service.gov.uk/firefox-with-rinetd:76.0.1-latest"
+
+DEFAULT_BROWSER=remote-chrome
+BROWSER_TYPE=$1
+
+if [ -z "$BROWSER_TYPE" ]; then
+    echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
+    echo ""
+fi
+
+if [ "${BROWSER_TYPE}" = "remote-firefox" ]; then
+  BROWSER="artefacts.tax.service.gov.uk/firefox-with-rinetd:76.0.1-latest"
+elif [ "${BROWSER_TYPE:=$DEFAULT_BROWSER}" = "remote-chrome" ]; then
+  BROWSER="artefacts.tax.service.gov.uk/chrome-with-rinetd:83.0.4103.61-latest"
 else
-  echo "ERROR: Browser type not recognised. Re-run the script with the option remote-chrome or remote-firefox."
+  echo "${BROWSER_TYPE} is unknown, exiting..."
   exit 1
 fi
 
 #######################################
-# Pulls the CONTAINER image from artifactory and runs the container with the specified options.
+# Pulls the BROWSER image from artifactory and runs the container with the specified options.
 #
 # Accepted Environment Variables:
 # PORT_MAPPINGS: List of the ports of the services under test.
@@ -55,11 +59,13 @@ fi
 # When using on a Linux OS, add "--net=host" to the docker run command.
 #######################################
 
-docker pull ${CONTAINER} &&
-  docker run -d --rm --name "${BROWSER_TYPE}" \
-    -p 4444:4444 -p 5900:5900 \
-    -e PORT_MAPPINGS="$port_mappings" \
-    -e TARGET_IP='host.docker.internal' \
-    ${CONTAINER} &&
-  docker run -d --rm --name "zap-${ZAP_VERSION}" \
-  -p 11000:11000 ${ZAP_CONTAINER}
+docker pull ${BROWSER} \
+  && docker run \
+  -d \
+  --rm \
+  --name "${BROWSER_TYPE:=$DEFAULT_BROWSER}" \
+  -p 4444:4444 \
+  -p 5900:5900 \
+  -e PORT_MAPPINGS="$port_mappings" \
+  -e TARGET_IP='host.docker.internal' \
+  ${BROWSER}
