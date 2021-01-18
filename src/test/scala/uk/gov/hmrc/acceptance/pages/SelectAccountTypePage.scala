859e4156de859d5221d24f3361afb9b9d278af79
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2021-01-18 11:45:25 +0000

    #TAV-210 Update locators

--- src/test/scala/uk/gov/hmrc/acceptance/pages/SelectAccountTypePage.scala
+++ src/test/scala/uk/gov/hmrc/acceptance/pages/SelectAccountTypePage.scala
@@ -4,8 +4,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions.titleIs
 
 case class SelectAccountTypePage() extends BasePage {
 
-  private lazy val personalAccount: RadioButton = radioButton(id("personal"))
-  private lazy val businessAccount: RadioButton = radioButton(id("business"))
+  private lazy val businessAccount: RadioButton = radioButton(id("accountType"))
+  private lazy val personalAccount: RadioButton = radioButton(id("accountType-2"))
   private lazy val continueButton: IdQuery = id("continue")
 
   def selectPersonalAccount(): SelectAccountTypePage = {
