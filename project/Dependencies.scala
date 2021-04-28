f10b6e29e6cea0fb2f0f6c14e9afae107647e8b5
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2021-04-28 10:52:46 +0100

    NOJIRA Add missing play json dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,8 +9,9 @@ object Dependencies {
     "com.vladsch.flexmark"    % "flexmark-all"        % "0.35.10"   %   Test,
     "com.github.nscala-time"  %% "nscala-time"        % "2.24.0"    %   Test,
     "uk.gov.hmrc"             %% "webdriver-factory"  % "0.+"       %   Test,
+    "com.typesafe.play"       %% "play-json"          % "2.6.13"    %   Test,
     "io.findify"              %% "s3mock"             % "0.2.6"     %   Test,
     "org.mock-server"         % "mockserver-netty"    % "5.11.1"    %   Test,
-    "org.assertj"             % "assertj-core"        % "3.18.0"     %   Test
+    "org.assertj"             % "assertj-core"        % "3.18.0"    %   Test
   )
 }
