0bcefcb249173efcc1c2f52acd30d68f1b583721
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2021-12-10 15:30:48 +0000

    #TAV-164 Fix override tests

--- src/test/scala/uk/gov/hmrc/acceptance/spec/v2/InitSpec.scala
+++ src/test/scala/uk/gov/hmrc/acceptance/spec/v2/InitSpec.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.acceptance.spec.v2
 
 import org.assertj.core.api.Assertions.assertThat
-import play.api.libs.json.Json
+import play.api.libs.json.{JsString, Json}
 import uk.gov.hmrc.acceptance.models._
 import uk.gov.hmrc.acceptance.models.init.InitRequest
 import uk.gov.hmrc.acceptance.pages.bavfe.{BusinessAccountEntryPage, PersonalAccountEntryPage, SelectAccountTypePage}
@@ -82,14 +82,14 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the English language copy overrides are displayed")
 
-    assertThat(BusinessAccountEntryPage().getHeading).isEqualTo(english("label.accountDetails.heading.business"))
-    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isEqualTo(english("label.accountName.business"))
-    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isEqualTo(english("label.sortCode.business"))
-    assertThat(BusinessAccountEntryPage().getSortCodeHint).isEqualTo(english("label.accountNumber.business"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isEqualTo(english("label.rollNumber.optional.business"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isEqualTo(english("hint.sortCode.business"))
-    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isEqualTo(english("hint.accountNumber.business"))
-    assertThat(BusinessAccountEntryPage().getRollNumberHint).isEqualTo(english("hint.rollNumber.business"))
+    assertThat(BusinessAccountEntryPage().getHeading).isEqualTo(english("label.accountDetails.heading.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isEqualTo(english("label.accountName.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isEqualTo(english("label.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeHint).isEqualTo(english("hint.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isEqualTo(english("label.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isEqualTo(english("hint.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isEqualTo(english("label.rollNumber.optional.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberHint).isEqualTo(english("hint.rollNumber.business").asInstanceOf[JsString].value)
 
     When("the language is switched to Welsh")
 
@@ -97,14 +97,14 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the Welsh copy overrides are displayed")
 
-    assertThat(BusinessAccountEntryPage().getHeading).isEqualTo(welsh("label.accountDetails.heading.business"))
-    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isEqualTo(welsh("label.accountName.business"))
-    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isEqualTo(welsh("label.sortCode.business"))
-    assertThat(BusinessAccountEntryPage().getSortCodeHint).isEqualTo(welsh("label.accountNumber.business"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isEqualTo(welsh("label.rollNumber.optional.business"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isEqualTo(welsh("hint.sortCode.business"))
-    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isEqualTo(welsh("hint.accountNumber.business"))
-    assertThat(BusinessAccountEntryPage().getRollNumberHint).isEqualTo(welsh("hint.rollNumber.business"))
+    assertThat(BusinessAccountEntryPage().getHeading).isEqualTo(welsh("label.accountDetails.heading.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isEqualTo(welsh("label.accountName.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isEqualTo(welsh("label.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeHint).isEqualTo(welsh("hint.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isEqualTo(welsh("label.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isEqualTo(welsh("hint.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isEqualTo(welsh("label.rollNumber.optional.business").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberHint).isEqualTo(welsh("hint.rollNumber.business").asInstanceOf[JsString].value)
 
     When("navigating to the personal account entry page")
 
@@ -115,14 +115,14 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the Welsh copy overrides are not displayed")
 
-    assertThat(PersonalAccountEntryPage().getHeading).isNotEqualTo(welsh("label.accountDetails.heading.business"))
-    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isNotEqualTo(welsh("label.accountName.business"))
-    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isNotEqualTo(welsh("label.sortCode.business"))
-    assertThat(PersonalAccountEntryPage().getSortCodeHint).isNotEqualTo(welsh("label.accountNumber.business"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isNotEqualTo(welsh("label.rollNumber.optional.business"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isNotEqualTo(welsh("hint.sortCode.business"))
-    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isNotEqualTo(welsh("hint.accountNumber.business"))
-    assertThat(PersonalAccountEntryPage().getRollNumberHint).isNotEqualTo(welsh("hint.rollNumber.business"))
+    assertThat(PersonalAccountEntryPage().getHeading).isNotEqualTo(welsh("label.accountDetails.heading.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isNotEqualTo(welsh("label.accountName.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isNotEqualTo(welsh("label.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeHint).isNotEqualTo(welsh("hint.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isNotEqualTo(welsh("label.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isNotEqualTo(welsh("hint.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isNotEqualTo(welsh("label.rollNumber.optional.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberHint).isNotEqualTo(welsh("hint.rollNumber.business").asInstanceOf[JsString].value)
 
     When("the language is switched back to English")
 
@@ -130,14 +130,14 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the English language copy overrides are not displayed")
 
-    assertThat(PersonalAccountEntryPage().getHeading).isNotEqualTo(english("label.accountDetails.heading.business"))
-    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isNotEqualTo(english("label.accountName.business"))
-    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isNotEqualTo(english("label.sortCode.business"))
-    assertThat(PersonalAccountEntryPage().getSortCodeHint).isNotEqualTo(english("label.accountNumber.business"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isNotEqualTo(english("label.rollNumber.optional.business"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isNotEqualTo(english("hint.sortCode.business"))
-    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isNotEqualTo(english("hint.accountNumber.business"))
-    assertThat(PersonalAccountEntryPage().getRollNumberHint).isNotEqualTo(english("hint.rollNumber.business"))
+    assertThat(PersonalAccountEntryPage().getHeading).isNotEqualTo(english("label.accountDetails.heading.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isNotEqualTo(english("label.accountName.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isNotEqualTo(english("label.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeHint).isNotEqualTo(english("hint.sortCode.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isNotEqualTo(english("label.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isNotEqualTo(english("hint.accountNumber.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isNotEqualTo(english("label.rollNumber.optional.business").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberHint).isNotEqualTo(english("hint.rollNumber.business").asInstanceOf[JsString].value)
   }
 
   Scenario("Can override copy on the personal account entry page without overriding copy on the business account entry page") {
@@ -180,14 +180,14 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the copy overrides are displayed")
 
-    assertThat(PersonalAccountEntryPage().getHeading).isEqualTo(english("label.accountDetails.heading.personal"))
-    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isEqualTo(english("label.accountName.personal"))
-    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isEqualTo(english("label.sortCode.personal"))
-    assertThat(PersonalAccountEntryPage().getSortCodeHint).isEqualTo(english("label.accountNumber.personal"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isEqualTo(english("label.rollNumber.optional.personal"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isEqualTo(english("hint.sortCode.personal"))
-    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isEqualTo(english("hint.accountNumber.personal"))
-    assertThat(PersonalAccountEntryPage().getRollNumberHint).isEqualTo(english("hint.rollNumber.personal"))
+    assertThat(PersonalAccountEntryPage().getHeading).isEqualTo(english("label.accountDetails.heading.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isEqualTo(english("label.accountName.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isEqualTo(english("label.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeHint).isEqualTo(english("hint.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isEqualTo(english("label.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isEqualTo(english("hint.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isEqualTo(english("label.rollNumber.optional.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberHint).isEqualTo(english("hint.rollNumber.personal").asInstanceOf[JsString].value)
 
     When("the language is switched to Welsh")
 
@@ -195,14 +195,14 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the Welsh copy overrides are displayed")
 
-    assertThat(PersonalAccountEntryPage().getHeading).isEqualTo(welsh("label.accountDetails.heading.personal"))
-    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isEqualTo(welsh("label.accountName.personal"))
-    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isEqualTo(welsh("label.sortCode.personal"))
-    assertThat(PersonalAccountEntryPage().getSortCodeHint).isEqualTo(welsh("label.accountNumber.personal"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isEqualTo(welsh("label.rollNumber.optional.personal"))
-    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isEqualTo(welsh("hint.sortCode.personal"))
-    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isEqualTo(welsh("hint.accountNumber.personal"))
-    assertThat(PersonalAccountEntryPage().getRollNumberHint).isEqualTo(welsh("hint.rollNumber.personal"))
+    assertThat(PersonalAccountEntryPage().getHeading).isEqualTo(welsh("label.accountDetails.heading.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNameLabel).isEqualTo(welsh("label.accountName.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeLabel).isEqualTo(welsh("label.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getSortCodeHint).isEqualTo(welsh("hint.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberLabel).isEqualTo(welsh("label.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getAccountNumberHint).isEqualTo(welsh("hint.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberLabel).isEqualTo(welsh("label.rollNumber.optional.personal").asInstanceOf[JsString].value)
+    assertThat(PersonalAccountEntryPage().getRollNumberHint).isEqualTo(welsh("hint.rollNumber.personal").asInstanceOf[JsString].value)
 
     When("navigating to the personal account entry page")
 
@@ -213,14 +213,14 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the Welsh copy overrides are not displayed")
 
-    assertThat(BusinessAccountEntryPage().getHeading).isNotEqualTo(welsh("label.accountDetails.heading.personal"))
-    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isNotEqualTo(welsh("label.accountName.personal"))
-    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isNotEqualTo(welsh("label.sortCode.personal"))
-    assertThat(BusinessAccountEntryPage().getSortCodeHint).isNotEqualTo(welsh("label.accountNumber.personal"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isNotEqualTo(welsh("label.rollNumber.optional.personal"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isNotEqualTo(welsh("hint.sortCode.personal"))
-    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isNotEqualTo(welsh("hint.accountNumber.personal"))
-    assertThat(BusinessAccountEntryPage().getRollNumberHint).isNotEqualTo(welsh("hint.rollNumber.personal"))
+    assertThat(BusinessAccountEntryPage().getHeading).isNotEqualTo(welsh("label.accountDetails.heading.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isNotEqualTo(welsh("label.accountName.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isNotEqualTo(welsh("label.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeHint).isNotEqualTo(welsh("hint.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isNotEqualTo(welsh("label.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isNotEqualTo(welsh("hint.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isNotEqualTo(welsh("label.rollNumber.optional.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberHint).isNotEqualTo(welsh("hint.rollNumber.personal").asInstanceOf[JsString].value)
 
     When("the language is switched back to English")
 
@@ -228,13 +228,13 @@ class InitSpec extends BaseSpec with MockServer {
 
     Then("the English language copy overrides are not displayed")
 
-    assertThat(BusinessAccountEntryPage().getHeading).isNotEqualTo(english("label.accountDetails.heading.personal"))
-    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isNotEqualTo(english("label.accountName.personal"))
-    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isNotEqualTo(english("label.sortCode.personal"))
-    assertThat(BusinessAccountEntryPage().getSortCodeHint).isNotEqualTo(english("label.accountNumber.personal"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isNotEqualTo(english("label.rollNumber.optional.personal"))
-    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isNotEqualTo(english("hint.sortCode.personal"))
-    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isNotEqualTo(english("hint.accountNumber.personal"))
-    assertThat(BusinessAccountEntryPage().getRollNumberHint).isNotEqualTo(english("hint.rollNumber.personal"))
+    assertThat(BusinessAccountEntryPage().getHeading).isNotEqualTo(english("label.accountDetails.heading.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getCompanyNameLabel).isNotEqualTo(english("label.accountName.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeLabel).isNotEqualTo(english("label.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getSortCodeHint).isNotEqualTo(english("hint.sortCode.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberLabel).isNotEqualTo(english("label.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getAccountNumberHint).isNotEqualTo(english("hint.accountNumber.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberLabel).isNotEqualTo(english("label.rollNumber.optional.personal").asInstanceOf[JsString].value)
+    assertThat(BusinessAccountEntryPage().getRollNumberHint).isNotEqualTo(english("hint.rollNumber.personal").asInstanceOf[JsString].value)
   }
 }
