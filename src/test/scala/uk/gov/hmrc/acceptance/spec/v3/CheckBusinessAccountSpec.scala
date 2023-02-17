f2939bfa71e85e13ea02995f76f2545d43fdce35
berlin-hmrc <berlin.bernadin1@digital.hmrc.gov.uk>
2023-02-17 12:32:04 +0000

    Fixed failing tests following the ticket TAV-722

--- src/test/scala/uk/gov/hmrc/acceptance/spec/v3/CheckBusinessAccountSpec.scala
+++ src/test/scala/uk/gov/hmrc/acceptance/spec/v3/CheckBusinessAccountSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -159,7 +159,7 @@ class CheckBusinessAccountSpec extends BaseSpec with MockServer {
           JsonPathBody.jsonPath(
             "$[?(" +
               "@.auditType=='businessBankAccountCheck' " +
-              "&& @.detail.length()==5" +
+              "&& @.detail.length()==6" +
               "&& @.detail.userAgent=='bank-account-verification-frontend'" +
               s"&& @.detail.callingClient=='$DEFAULT_SERVICE_IDENTIFIER'" +
               "&& @.detail.context=='surepay_business_succeeded'" +
