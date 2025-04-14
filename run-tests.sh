3901c1256947048ae93457fe3478268988fdf94b
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-04-14 18:23:33 +0100

    CIR-2033 revert run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,4 @@
 BROWSER=$1
 ENVIRONMENT=$2
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.ui.specs.v3.StrideCheckPersonalAccountSpec" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.ui.specs.*" testReport
