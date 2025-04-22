9ec6cf1c1522bc133e4119a6f38d384455f73926
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-04-22 14:15:03 +0100

    CIR-2033 Switch to Adobe S3 mock

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,11 @@ object Dependencies {
     "uk.gov.hmrc"         %% "ui-test-runner"         % "0.45.0"   % Test,
     "com.typesafe.play"   %% "play-json"              % "2.10.6"   % Test,
     "com.typesafe.play"   %% "play-ahc-ws-standalone" % "2.2.11"   % Test,
-    "io.findify"          %% "s3mock"                 % "0.2.6"    % Test,
+    "com.adobe.testing"    % "s3mock"                 % "4.1.1"    % Test,
+    "org.eclipse.jetty"    % "jetty-io"               % "12.0.19"  % Test,
+    "org.eclipse.jetty"    % "jetty-client"           % "12.0.19"  % Test,
+    "org.eclipse.jetty"    % "jetty-http"             % "12.0.19"  % Test,
+    "org.eclipse.jetty"    % "jetty-util"             % "12.0.19"  % Test,
     "org.mock-server"      % "mockserver-netty"       % "5.15.0"   % Test,
     "org.assertj"          % "assertj-core"           % "3.27.3"   % Test
   )
